53b5c1ea363a60209c78344de128fd336560ea0c
Pier0Rz <166246096+Pier0RR@users.noreply.github.com>
2024-04-20 14:11:04 -0500

    Add files via upload

---
+++ src/sesion4/Sesion4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion4;
+
+import sesion4.pantallas.PantallaCalculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Sesion4 {
+
+
+    public static void main(String[] args) {
+        PantallaCalculadora pantalla= new PantallaCalculadora();
+        pantalla.setVisible(true);
+    }
+    
+}
